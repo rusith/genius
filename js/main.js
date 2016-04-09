34d403010709a36de143edea1c61882e537e4edc
rusith <srlsooriyagoda@gmail.com>
2016-04-09 22:15:28 +0530

    added a comment section

--- js/main.js
+++ js/main.js
@@ -22,4 +22,17 @@ function setDocunloadCount()
 	});
 }
 
+// function initializeComments()
+// {
+// 	this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+//   this.page.identifier = PAGE_IDENTIFIER; 
+// 	var d = document, s = d.createElement('script');
+    
+//   s.src = 'https://geniusclipboardmanager.disqus.com/embed.js';
+  
+//   s.setAttribute('data-timestamp', +new Date());
+//   (d.head || d.body).appendChild(s);
+// }
+
 setDocunloadCount();
+//initializeComments();
